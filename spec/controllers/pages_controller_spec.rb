388b40ca69664f3e3973154846475051d43876a8
Ian DesJardins <idesjardins@sazboom.com>
2012-07-03 11:11:46 -0400

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,7 +12,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title", 
-        	:content => "Ruby on Rails Tutorial Sample App | Home")
+        	:content => "Home")
     end
   end
 
@@ -25,7 +25,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title", 
-        	:content => "Ruby on Rails Tutorial Sample App | Contact")
+        	:content => "Contact")
     end
   end
   
@@ -38,7 +38,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title", 
-        	:content => "Ruby on Rails Tutorial Sample App | About")
+        	:content => "About")
     end
   end
 end
